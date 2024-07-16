d43b75a9bf6d43bba84184a9fe581e5a49c74134
LexKRS <lexocode@yandex.ru>
2024-07-16 11:33:36 +0300

    Added card, cardList, Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
+</script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <header class="flex justify-between border-b border-slate-300 px-8">
-      <div class="flex items-center gap-4">
-        <img src="/logo.png" alt="logo" class="w-10 mr-5" />
-        <div>
-          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
-          <p class="text-slate-400">Магазин лучших кроссовок</p>
-        </div>
-      </div>
-
-      <ul class="flex items-center gap-10">
-        <li class="flex items-center gap-3">
-          <img class="" src="/cart.svg" alt="cart" />
-          <b>1205 руб.</b>
-        </li>
-        <li class="flex items-center gap-3">
-          <img class="" src="/heart.svg" alt="heart" />
-          <b>Закладки</b>
-        </li>
-        <li class="flex items-center gap-3">
-          <img class="" src="/profile.svg" alt="profile" />
-          <b>Профиль</b>
-        </li>
-      </ul>
-    </header>
+  <Drawer />
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Header />
+    <CardList />
   </div>
 </template>
 
---
+++ src/components/CardList.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClik = () => {
+  alert('Added in favorite!')
+}
+</script>
+
+<template>
+  <div class="p-10">
+    <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <Card
+        imageUrl="/public/sneakers/sneakers-1.jpg"
+        title="Мужские Кроссовки Nike Blazer Mid Suede"
+        :price="12599"
+        :onClickFavorite="onClik"
+      />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
+</template>
